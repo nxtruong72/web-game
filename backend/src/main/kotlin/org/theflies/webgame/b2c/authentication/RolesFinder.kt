4864f7e0be1076c67fe950006a2bcc01be698b3a
gaconkzk <gaconkzk@gmail.com>
2024-01-03 10:51:53 +0700

    delete unuse

--- backend/src/main/kotlin/org/theflies/webgame/b2c/authentication/RolesFinder.kt
+++
@@ -1,9 +0,0 @@
-package org.theflies.webgame.b2c.authentication
-
-import io.micronaut.security.token.RolesFinder
-
-class RolesFinder: RolesFinder {
-  override fun resolveRoles(attributes: MutableMap<String, Any>?): MutableList<String> {
-    TODO("Not yet implemented")
-  }
-}
