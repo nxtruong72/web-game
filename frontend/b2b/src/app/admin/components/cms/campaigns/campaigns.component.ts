82e05a605faadb74769f1250dba6efa1579daca8
tc0678 <155502320+tc0678@users.noreply.github.com>
2024-01-16 22:31:05 +0700

    FE-SC: Create cms mockup #2

---
+++ frontend/b2b/src/app/admin/components/cms/campaigns/campaigns.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-campaigns',
+  templateUrl: './campaigns.component.html',
+  styleUrls: ['./campaigns.component.scss'],
+  standalone: true,
+})
+export class CampaignsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ frontend/b2b/src/app/admin/components/cms/game-bank/game-bank.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-bank',
+  templateUrl: './game-bank.component.html',
+  styleUrls: ['./game-bank.component.scss'],
+  standalone: true,
+})
+export class GameBankComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
