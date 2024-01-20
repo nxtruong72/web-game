65a39610e2b1dc666da97fd7bbfda535315a2c21
tc0678 <155502320+tc0678@users.noreply.github.com>
2024-01-20 15:55:34 +0700

    FE-SC: add forgot password form
    FE-SC: add forgot password form

--- frontend/b2c/src/app/authentication/components/forgot-password/forgot-password.component.ts
+++ frontend/b2c/src/app/authentication/components/forgot-password/forgot-password.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss'],
   standalone: true,
+  imports: [CommonModule, RouterLink],
 })
 export class ForgotPasswordComponent implements OnInit {
   constructor() {}
