0281fb7eba62e0a99a0515cd68b76178bc66b482
tc0678 <155502320+tc0678@users.noreply.github.com>
2024-03-12 22:27:08 +0700

    FE-SC: fix scroll

--- frontend/b2b/src/app/admin/service/betting-odds.service.ts
+++ frontend/b2b/src/app/admin/service/betting-odds.service.ts
@@ -13,8 +13,9 @@ export class BettingOddsService {
   createGame(): Observable<IBettingOdds> {
     return this._bettingOddsApiService.getGames().pipe(
       map((games: IBettingOdds) => {
-        games.content = [
-          {
+
+        [...Array(50).keys()].forEach(() => {
+          games.content.push({
             id: getStandardValue('02122023'),
             name: getStandardValue('AOE IAC: TA1 - SHS P5'),
             form: getStandardValue('Cung - Chém'),
@@ -22,17 +23,9 @@ export class BettingOddsService {
             startDate: getStandardValue('30/12/2023 09:00:00'),
             total: getStandardValue(null),
             profit: getStandardValue(null),
-          },
-          {
-            id: getStandardValue('02122023'),
-            name: getStandardValue('AOE IAC: TA1 - SHS P5'),
-            form: getStandardValue('Cung - Chém'),
-            status: getStandardValue('Sắp diễn ra'),
-            startDate: getStandardValue('30/12/2023 09:00:00'),
-            total: getStandardValue(null),
-            profit: getStandardValue(null),
-          },
-        ];
+          },)  
+        })
+
         return games;
       }),
     );
